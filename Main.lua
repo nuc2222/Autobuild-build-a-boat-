9b0964dc0fc4bac9031263f474a75c51332e4676
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-15 13:02:04 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 -- declsre main functions.
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "build a boat autobuild", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "build a boat scripts", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 keya = loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Keysystem/Getkeyapi.lua"))()
 local autobuildscriptTab = Window:MakeTab({
 	Name = "autobuild babft",
@@ -21,6 +21,24 @@ autobuildscriptTab:AddTextbox({
 	Callback = function(key)
 		if key == keya then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Scripts/Autobuild.lua"))()
+		else
+			OrionLib:MakeNotification({
+				Name = "key system",
+				Content = "uh oh it seems youve entered the incorrect key",
+				Image = "rbxassetid://4483345998",
+				Time = 5
+                        })
+		end
+
+	end	  
+})
+vvahubTab:AddTextbox({
+	Name = "enter key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(key)
+		if key == keya then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Scripts/vvahub.lua"))()
 		else
 			OrionLib:MakeNotification({
 				Name = "key system",
