c9ae33714552f1d19b00fce6b3c886864efc2ea4
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-15 14:49:25 +0200

    Update Vvahub.lua

--- Data/Scripts/Vvahub.lua
+++ Data/Scripts/Vvahub.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+lollocal OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder
 
 local homeTab = Window:MakeTab({
@@ -20,4 +20,26 @@ local othersTab = Window:MakeTab({
 	Name = "others",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+mainTab:AddToggle({
+	Name = "autofarm gold block",
+	Default = false,
+	Callback = function(Value)
+		while true do
+			if Value == true then
+				local player = game.Players.LocalPlayer
+				local character = player.Character or player.CharacterAdded:Wait()
+				local destination = Vector3.new(-51.5656, 65, 5989.09)
+				character:MoveTo(destination)
+				wait(2)
+				local player = game.Players.LocalPlayer
+				local character = player.Character or player.CharacterAdded:Wait()
+				local destination = Vector3.new(-51.5656, -100, 9498)
+				character:MoveTo(destination)
+				wait(5)
+			else
+				break
+			end
+		end
+	end    
 })
