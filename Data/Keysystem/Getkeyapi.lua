b41322eaf002d05d05cca4efb0acb8a123068b72
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-15 11:22:48 +0200

    Update Getkeyapi.lua

--- Data/Keysystem/Getkeyapi.lua
+++ Data/Keysystem/Getkeyapi.lua
@@ -1,12 +1,13 @@
 local currentDate = os.date("*t")
 local keys = {
-    [1] = "hrjehchsjenebdcmd"
+    [1] = "hrjehchsjenebdcmd",
     [2] = "idjxmdneiakxsmeiwkd",
     [3] = "owlsmrkeplsplsplsksk",
     [4] = "mdkanedjsbsbebsnzd",
     [5] = "oemfkdkadkdmekskd",
     [6] = "mejxjfujsnenejsjdjsns",
     [7] = "kelwopdjeksozlskadm",
-}
+  }
 local currentDay = currentDate.wday
-Return keys[currentDay]
+local key = keys[currentDay]
+return key
