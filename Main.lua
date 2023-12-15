7b7eee18551ea3b553571fa626f0a77c11ca2a6c
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-15 12:13:52 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,27 +1,35 @@
--- declsre main functions.
+-- declsre main functions
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "build a boat autobuild", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-keya = loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Keysystem/Getkeyapi.lua I am "))()
+keya = loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Keysystem/Getkeyapi.lua"))()
 local autobuildscriptTab = Window:MakeTab({
-	Name = "Tab 1",
+	Name = "autobuild babft",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
 local vvahubTab = Window:MakeTab({
-	Name = "Tab 1",
+	Name = "babft gui",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-autobuildscript:AddTextbox({
+autobuildscriptTab:AddTextbox({
 	Name = "enter key",
 	Default = "",
 	TextDisappear = true,
 	Callback = function(key)
 		if key == keya then
-				loadstring(game:HttpGet('https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Scripts/Autobuild.lua'))()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Autobuild-build-a-boat-/main/Data/Scripts/Autobuild.lua"))()
+		else then
+			OrionLib:MakeNotification({
+				Name = "key system",
+				Content = "uh oh it seems youve entered the incorrect key",
+				Image = "rbxassetid://4483345998",
+				Time = 5
+                        })
 		end
+
 	end	  
 })
 OrionLib:Init()
