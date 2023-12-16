c24a8c177dffb68f6fee7600149b842efccb1b3d
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-16 11:06:55 +0200

    Update Vvahub.lua

--- Data/Scripts/Vvahub.lua
+++ Data/Scripts/Vvahub.lua
@@ -1,5 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Vva babft hub", HidePremium = false, SaveConfig = true, ConfigFolder})
+local player = game.Players.LocalPlayer
+local playerName = player.Name
 
 local homeTab = Window:MakeTab({
 	Name = "home",
@@ -25,25 +27,35 @@ local othersTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+homeTab:AddButton({
+	Name = "destroy gui",
+	Callback = function()
+      		OrionLib:MakeNotification({
+			Name = "goodbye",
+			Content = "destroying gui...",
+			Image = "rbxassetid://4483345998",
+			Time = 3
+		})
+                wait(3)
+		OrionLib:Destroy()
+  	end    
+})
+
 mainTab:AddToggle({
 	Name = "autofarm gold block",
 	Default = false,
 	Callback = function(Value)
 		while Value do
-			if True then
-				local player = game.Players.LocalPlayer
-				local character = player.Character or player.CharacterAdded:Wait()
-				local destination = Vector3.new(-51.5656, 65, 5989)
-				character:MoveTo(destination)
-				wait(2)
-				local player = game.Players.LocalPlayer
-				local character = player.Character or player.CharacterAdded:Wait()
-				local destination = Vector3.new(-51.5656, -100, 9498)
-				character:MoveTo(destination)
-				wait(5)
-			else
-				break
-			end
+			local player = game.Players.LocalPlayer
+			local character = player.Character or player.CharacterAdded:Wait()
+			local destination = Vector3.new(-51.5656, 65, 5989)
+			character:MoveTo(destination)
+			wait(2)
+			local player = game.Players.LocalPlayer
+			local character = player.Character or player.CharacterAdded:Wait()
+			local destination = Vector3.new(-51.5656, -100, 9498)
+			character:MoveTo(destination)
+			wait(10)
 		end
 	end    
 })
