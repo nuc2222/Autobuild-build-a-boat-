159947eec108fc4674d35f9127516ec8dbf4d575
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-12-16 10:36:19 +0200

    Update Vvahub.lua

--- Data/Scripts/Vvahub.lua
+++ Data/Scripts/Vvahub.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Vva babft hub", HidePremium = false, SaveConfig = true, ConfigFolder
+local Window = OrionLib:MakeWindow({Name = "Vva babft hub", HidePremium = false, SaveConfig = true, ConfigFolder})
 
 local homeTab = Window:MakeTab({
 	Name = "home",
